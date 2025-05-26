660047287a4c011221d13b76c67a7fbb5d6aa6dd
Pukkok <krystalgggg@gmail.com>
2025-05-26 10:36:52 +0900

    [Feat] 페이지 사이즈 객체 생성

---
+++ src/utils/paperSizes.js
@@ -0,0 +1,12 @@
+export const PAPER_SIZES = {
+  A1: { width: 7016, height: 9933, mm: '594×841' },
+  A2: { width: 4961, height: 7016, mm: '420×594' },
+  A3: { width: 3508, height: 4961, mm: '297×420' },
+  A4: { width: 2480, height: 3508, mm: '210×297' },
+  A5: { width: 1748, height: 2480, mm: '148×210' },
+  B1: { width: 7291, height: 10302, mm: '707×1000' },
+  B2: { width: 5157, height: 7291, mm: '500×707' },
+  B3: { width: 3544, height: 5157, mm: '353×500' },
+  B4: { width: 2500, height: 3544, mm: '250×353' },
+  B5: { width: 1754, height: 2500, mm: '176×250' }
+}
