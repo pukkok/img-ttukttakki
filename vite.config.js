7181bc6c2fd2f6e1c234570316020f9cfa375fc5
Pukkok <krystalgggg@gmail.com>
2025-07-09 19:33:09 +0900

    [Fix] fabric 설정 변경 다운그레이드

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/img-ttukttakki/',
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['fabric']
+  },
 })
