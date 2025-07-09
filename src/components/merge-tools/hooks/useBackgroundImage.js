92a93377c777362229fd40422396add5dd325ff7
Pukkok <krystalgggg@gmail.com>
2025-07-09 19:06:20 +0900

    [Fix] 캔버스 배경이미지 정확한 역할 분리

--- src/components/merge-tools/hooks/useBackgroundImage.js
+++ src/components/merge-tools/hooks/useBackgroundImage.js
@@ -1,74 +1,104 @@
 import { useEffect } from 'react'
-import { FabricImage } from 'fabric'
+import { fabric } from 'fabric'
 import { useMergeCanvasStore } from '../../../stores/useMergeCanvasStore'
 
 export const useBackgroundImage = (fabricCanvasRef) => {
   const backgroundImage = useMergeCanvasStore(s => s.backgroundImageUrl)
-  const isFixBackground = useMergeCanvasStore(s => s.isFixBackground)
+  const isLockBackground = useMergeCanvasStore(s => s.isLockBackground)
   const setBackgroundImageInfo = useMergeCanvasStore(s => s.setBackgroundImageInfo)
 
+  // INFO: 배경이벤트 등록
   useEffect(() => {
-    const loadBackground = async () => {
-      const canvas = fabricCanvasRef.current
-      if (!canvas) return
+    const canvas = fabricCanvasRef.current
+    if (!canvas) return
 
-      const existingBg = canvas.getObjects().find(obj => obj.customType === 'background')
-      if (existingBg) canvas.remove(existingBg)
+    const handleModified = (e) => {
+      const obj = e.target
+      if (obj?.get('customType') !== 'background') return
 
-      if (!backgroundImage) {
-        setBackgroundImageInfo(null)
-        canvas.renderAll()
-        return
-      }
-
-      try {
-        const bgImg = await FabricImage.fromURL(backgroundImage)
-        const canvasWidth = canvas.getWidth()
-        const canvasHeight = canvas.getHeight()
-
-        const targetWidth = (canvasWidth * 2) / 3
-        const scale = targetWidth / bgImg.width
-        const scaledWidth = bgImg.width * scale
-        const scaledHeight = bgImg.height * scale
-        const left = (canvasWidth - scaledWidth) / 2
-        const top = (canvasHeight - scaledHeight) / 2
-
-        bgImg.set({
-          left,
-          top,
-          scaleX: scale,
-          scaleY: scale,
-          angle: 0,
-          skewX: 0,
-          skewY: 0,
-          selectable: true,
-          evented: true,
-          hasControls: true,
-          customType: 'background',
-        })
-
-        canvas.add(bgImg)
-
-        setBackgroundImageInfo({
-          width: scaledWidth,
-          height: scaledHeight,
-          left,
-          top,
-          rotation: 0,
-          skewX: 0,
-          skewY: 0,
-        })
-
-        canvas.renderAll()
-      } catch (err) {
-        console.error('배경 이미지 불러오기 실패:', err)
-        setBackgroundImageInfo(null)
-      }
+      setBackgroundImageInfo({
+        width: obj.width * obj.scaleX,
+        height: obj.height * obj.scaleY,
+        left: obj.left,
+        top: obj.top,
+        rotation: obj.angle || 0,
+        skewX: obj.skewX || 0,
+        skewY: obj.skewY || 0,
+      })
     }
 
-    loadBackground()
+    canvas.on('object:modified', handleModified)
+    canvas.on('object:moving', handleModified)
+    canvas.on('object:scaling', handleModified)
+    canvas.on('object:rotating', handleModified)
+
+    return () => {
+      canvas.off('object:modified', handleModified)
+      canvas.off('object:moving', handleModified)
+      canvas.off('object:scaling', handleModified)
+      canvas.off('object:rotating', handleModified)
+    }
+  }, [])
+
+  // INFO: 배경 넣기
+  useEffect(() => {
+    const canvas = fabricCanvasRef.current
+    if (!canvas) return
+
+    // INFO: 이미 배경이 들어있다면 지운다.
+    const existingBg = canvas.getObjects().find(obj => obj.customType === 'background')
+    if (existingBg) canvas.remove(existingBg)
+
+    if (!backgroundImage) { // INFO: 배경 삽입이 잘못된 경우
+      setBackgroundImageInfo(null)
+      canvas.renderAll()
+      return
+    }
+
+    fabric.Image.fromURL(backgroundImage, (bgImg) => {
+      if (!bgImg) return
+
+      const canvasWidth = canvas.getWidth()
+      const canvasHeight = canvas.getHeight()
+
+      const targetHeight = (canvasHeight * 2) / 3
+      const scale = targetHeight / bgImg.height
+      const scaledWidth = bgImg.width * scale
+      const scaledHeight = bgImg.height * scale
+      const left = (canvasWidth - scaledWidth) / 2
+      const top = (canvasHeight - scaledHeight) / 2
+
+      bgImg.set({
+        left,
+        top,
+        scaleX: scale,
+        scaleY: scale,
+        angle: 0,
+        skewX: 0,
+        skewY: 0,
+        selectable: true,
+        evented: true,
+        hasControls: true,
+        customType: 'background',
+      })
+
+      canvas.add(bgImg)
+      setBackgroundImageInfo({
+        width: scaledWidth,
+        height: scaledHeight,
+        left,
+        top,
+        rotation: 0,
+        skewX: 0,
+        skewY: 0,
+      })
+      
+      canvas.setActiveObject(bgImg)
+      canvas.requestRenderAll()
+    }, { crossOrigin: 'anonymous' }) // 이미지가 외부일 경우를 위해
   }, [backgroundImage])
 
+  // INFO: 배경 잠금시 처리
   useEffect(() => {
     const canvas = fabricCanvasRef.current
     if (!canvas) return
@@ -77,15 +107,15 @@ export const useBackgroundImage = (fabricCanvasRef) => {
     if (!bgObj) return
 
     bgObj.set({
-      selectable: !isFixBackground,
-      evented: !isFixBackground,
-      hasControls: !isFixBackground,
+      selectable: !isLockBackground,
+      evented: !isLockBackground,
+      hasControls: !isLockBackground,
     })
 
-    if (isFixBackground) {
+    if (isLockBackground) {
       canvas.discardActiveObject()
     }
-
+    console.log('동작해?')
     canvas.requestRenderAll()
-  }, [isFixBackground])
+  }, [isLockBackground])
 }
