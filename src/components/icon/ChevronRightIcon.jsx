3c4a4306a178ccc5961e3dd80c12eb06339d71d9
Pukkok <krystalgggg@gmail.com>
2025-09-16 00:29:40 +0900

    [Feat] 방향 아이콘 추가

---
+++ src/components/icon/ChevronRightIcon.jsx
@@ -0,0 +1,15 @@
+const ChevronRightIcon = ({ size = 20, className = '' }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className={className}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+  </svg>
+)
+
+export default ChevronRightIcon
